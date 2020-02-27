b02bf6ad0b3b7f97f240337d0a74df681f1b08f7
Nick Morales <bick.norales@gmail.com>
2020-02-27 12:56:59 -0800

    Updates

---
+++ precache-manifest.3a69ff6802332a2c669d9520d8c33159.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2746df32138920a60dc57bd88bf8ac",
+    "url": "/Bojack-Clicky-Game/index.html"
+  },
+  {
+    "revision": "60e5ff51283796cf84d9",
+    "url": "/Bojack-Clicky-Game/static/css/main.8014ba8a.chunk.css"
+  },
+  {
+    "revision": "0bc21264ffb2f8f261d0",
+    "url": "/Bojack-Clicky-Game/static/js/2.016dbaa6.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Bojack-Clicky-Game/static/js/2.016dbaa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e5ff51283796cf84d9",
+    "url": "/Bojack-Clicky-Game/static/js/main.81b45436.chunk.js"
+  },
+  {
+    "revision": "10c7b34ab7abc5912cee",
+    "url": "/Bojack-Clicky-Game/static/js/runtime-main.1311436f.js"
+  },
+  {
+    "revision": "69693e9ee69a6e63c9e8322596d59c44",
+    "url": "/Bojack-Clicky-Game/static/media/0.69693e9e.jpg"
+  },
+  {
+    "revision": "d1adf87c160cfae1798f849a34bb66a8",
+    "url": "/Bojack-Clicky-Game/static/media/1.d1adf87c.jpg"
+  },
+  {
+    "revision": "4c121079890b1d6d1b8563783f2f1497",
+    "url": "/Bojack-Clicky-Game/static/media/10.4c121079.jpg"
+  },
+  {
+    "revision": "bfa082661900c349d0ecaf1316e93b21",
+    "url": "/Bojack-Clicky-Game/static/media/11.bfa08266.jpg"
+  },
+  {
+    "revision": "421912371a1ce0fa105df73d3c21f2df",
+    "url": "/Bojack-Clicky-Game/static/media/2.42191237.jpg"
+  },
+  {
+    "revision": "1b06b0c3976e379fe4ffaa1914538715",
+    "url": "/Bojack-Clicky-Game/static/media/3.1b06b0c3.jpg"
+  },
+  {
+    "revision": "2b0498a36132f3f81f86efe0a7fc58e1",
+    "url": "/Bojack-Clicky-Game/static/media/4.2b0498a3.jpg"
+  },
+  {
+    "revision": "e48d24cff7a6b6667e772787a2edb496",
+    "url": "/Bojack-Clicky-Game/static/media/5.e48d24cf.jpg"
+  },
+  {
+    "revision": "a7ae85325ed49179c5a7f48c9f66f679",
+    "url": "/Bojack-Clicky-Game/static/media/6.a7ae8532.jpg"
+  },
+  {
+    "revision": "d2b0c715fd57edf33bddd9a82e7b82ef",
+    "url": "/Bojack-Clicky-Game/static/media/7.d2b0c715.jpg"
+  },
+  {
+    "revision": "5a82e315f72f17e9f76f4f59740e805d",
+    "url": "/Bojack-Clicky-Game/static/media/8.5a82e315.jpg"
+  },
+  {
+    "revision": "2ce35072eea16e323ac3f67fca7bdb42",
+    "url": "/Bojack-Clicky-Game/static/media/9.2ce35072.jpg"
+  }
+]);
